71a96959e15183222568436457ceefb6af60377c
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-23 00:16:10 +0600

    add: contact-data

---
+++ src/shared/data/ContactData.jsx
@@ -0,0 +1,24 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { MdAlternateEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+
+export const contactData = [
+  {
+    id: 1,
+    icon: <FaLocationDot />,
+    title: "Location",
+    name: "Mirpur 2, Dhaka, Bangladesh",
+  },
+  {
+    id: 2,
+    icon: <MdAlternateEmail />,
+    title: "Email",
+    name: "nirobsarker012@gmail.com",
+  },
+  {
+    id: 3,
+    icon: <FaPhone />,
+    title: "Phone",
+    name: "+8801854433053",
+  },
+];
