504f451ab837ef1754318de5b2cd3835ad1e4330
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-23 00:38:25 +0600

    added: funfact-data-and-github-calender

---
+++ src/shared/data/FunFactData.jsx
@@ -0,0 +1,22 @@
+export const funFactData = [
+  {
+    id: 1,
+    number: 2,
+    title: "PROJECTS DONE",
+  },
+  {
+    id: 2,
+    number: 1,
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 3,
+    number: "5+",
+    title: "HOURS OF WORK",
+  },
+  {
+    id: 4,
+    number: 0,
+    title: "AWARDS RECEIVED",
+  },
+];
