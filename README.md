b19f0d50c38afb32345b48daf6230fc8f5f0ce78
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-23 00:10:10 +0600

    add: blog-data

--- README.md
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "setup-configuration"
-git branch -M main
-git remote add origin https://github.com/nirobsarker012/my-main-portfolio.git
-git push -u origin main
--- src/shared/data/BlogData.jsx
+++ src/shared/data/BlogData.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
-const BlogData = () => {
-  return <div></div>;
-};
-
-export default BlogData;
+const blog1 =
+  "https://miro.medium.com/v2/resize:fit:720/format:webp/0*0x_eneXKWKWrgHtp";
+import blog2 from "../../assets/images/blogs_img/blog2.jpg";
+import blog3 from "../../assets/images/blogs_img/blog3.jpg";
+import blog4 from "../../assets/images/blogs_img/blog4.jpg";
+export const blogData = [
+  {
+    id: 1,
+    img: blog1,
+    title: "Linear Search in JavaScript",
+    date: " March 1, 2023",
+    comments: 0,
+    desc: "Linear search in an array returns true if any element matches the target, else false",
+    live_link:
+      "https://dev.to/mdqayyumshareef/linear-search-algorithm-javascript-mb4",
+    category: "Programming",
+  },
+  {
+    id: 2,
+    img: blog2,
+    title: "CREATE A FULL STACK WEBSITE",
+    date: " February 2, 2024",
+    comments: 5,
+    desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    // category: "Website",
+    category: "Upcoming",
+  },
+  {
+    id: 3,
+    img: blog3,
+    title: "TOP GRAPHIC DESIGN TOPICS",
+    date: " February 2, 2024",
+    comments: 5,
+    desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    // category: "Design",
+    category: "Upcoming",
+  },
+  {
+    id: 4,
+    img: blog4,
+    title: "TOP 10 TOPICS OF FRONTEND DEVELOPMENT",
+    date: " February 2, 2024",
+    comments: 5,
+    desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    // category: "Website",
+    category: "Upcoming",
+  },
+];
