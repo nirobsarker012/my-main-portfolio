a4aff077d829d8a59d4144e57f19e48dea946298
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-23 00:17:30 +0600

    add: feedback-data

---
+++ src/shared/data/FeedbackData.jsx
@@ -0,0 +1,20 @@
+export const feedbackData = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/3pjbxrL/mohi-uddin.jpg",
+    name: "Mohi Uddin",
+    position: "CEO",
+    company: "Devaspire",
+    fbMsg:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed id suscipit corrupti debitis provident inventore ullam, quae officiis. Totam, nisi?",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/3pjbxrL/mohi-uddin.jpg",
+    name: "Mohi Uddin",
+    position: "CEO",
+    company: "Devaspire",
+    fbMsg:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed id suscipit corrupti debitis provident inventore ullam, quae officiis. Totam, nisi?",
+  },
+];
