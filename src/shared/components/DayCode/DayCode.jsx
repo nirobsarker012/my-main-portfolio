20a5975c465c97c07e7915a8f30d4f059fc150de
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-23 13:27:29 +0600

    add: update-home and components-path

---
+++ src/shared/components/DayCode/DayCode.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DayCode = () => {
+  return <div></div>;
+};
+
+export default DayCode;
--- src/shared/pages/Home.jsx
+++ src/shared/pages/Home.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Banner from "../components/Banner/Banner";
+import About from "../components/About/About";
+import Skills from "../components/Skill/Skills";
+import DayCode from "../components/DayCode/DayCode";
+import Project from "../components/Project/Project";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer";
+import Blogs from "../components/Blogs/Blogs";
 
 const Home = () => {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <div className="min-h-screen w-full bg-black relative pop">
+        {/* Dark Noise Colored Background */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            background: "#000000",
+            backgroundImage: `
+        radial-gradient(circle at 1px 1px, rgba(139, 92, 246, 0.2) 1px, transparent 0),
+        radial-gradient(circle at 1px 1px, rgba(59, 130, 246, 0.18) 1px, transparent 0),
+        radial-gradient(circle at 1px 1px, rgba(236, 72, 153, 0.15) 1px, transparent 0)
+      `,
+            backgroundSize: "20px 20px, 30px 30px, 25px 25px",
+            backgroundPosition: "0 0, 10px 10px, 15px 5px",
+          }}
+        />
+        <Navbar />
+        <Banner />
+        <About />
+        <Skills />
+        <DayCode />
+        <Project />
+        <Blogs />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   );
 };
 
