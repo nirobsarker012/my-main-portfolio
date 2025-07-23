1df757302ab14ca5480689e8665e8da39bbda73a
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-23 13:04:17 +0600

    add: skill-data

---
+++ src/shared/data/SkillData.jsx
@@ -0,0 +1,169 @@
+// Frontend Data
+import html from "../../assets/images/skills_icons/html.png";
+import css from "../../assets/images/skills_icons/css.png";
+import js from "../../assets/images/skills_icons/javascript.png";
+import tailwind from "../../assets/images/skills_icons/tailwindcss.png";
+import bootstrap from "../../assets/images/skills_icons/bootstrap.png";
+import react from "../../assets/images/skills_icons/reactjs.png";
+import typescript from "../../assets/images/skills_icons/typescript.svg";
+import next from "../../assets/images/skills_icons/next.png";
+import firebase from "../../assets/images/skills_icons/firebase.png";
+import tanstack from "../../assets/images/skills_icons/tanstack-query.png";
+
+export const skillsFrontend = [
+  {
+    id: 1,
+    img: html,
+    name: "html",
+  },
+  {
+    id: 2,
+    img: css,
+    name: "css",
+  },
+
+  {
+    id: 3,
+    img: tailwind,
+    name: "tailwind",
+  },
+  {
+    id: 4,
+    img: bootstrap,
+    name: "bootstrap",
+  },
+  {
+    id: 5,
+    img: js,
+    name: "javascript",
+  },
+
+  {
+    id: 6,
+    img: react,
+    name: "react.js",
+  },
+  {
+    id: 7,
+    img: next,
+    name: "next.js",
+  },
+  {
+    id: 8,
+    img: typescript,
+    name: "typescript",
+  },
+  //   {
+  //     id: 9,
+  //     img: redux,
+  //     name: "redux",
+  //   },
+  {
+    id: 9,
+    img: tanstack,
+    name: "tanstack query",
+  },
+  //   {
+  //     id: 11,
+  //     img: zustand,
+  //     name: "zustand",
+  //   },
+
+  {
+    id: 10,
+    img: firebase,
+    name: "firebase",
+  },
+];
+
+// Backend Data
+import node from "../../assets/images/skills_icons/nodejs.png";
+import express from "../../assets/images/skills_icons/express.png";
+import mongoDB from "../../assets/images/skills_icons/mongodb.png";
+import mongoose_img from "../../assets/images/skills_icons/mongoose.png";
+
+export const skillsBackend = [
+  {
+    id: 1,
+    img: node,
+    name: "node.js",
+  },
+  {
+    id: 2,
+    img: express,
+    name: "express.js",
+  },
+  {
+    id: 3,
+    img: mongoDB,
+    name: "mongoDB",
+  },
+  {
+    id: 4,
+    img: mongoose_img,
+    name: "mongoose",
+  },
+];
+
+// Tools
+import git from "../../assets/images/skills_icons/git.png";
+import github from "../../assets/images/skills_icons/github.png";
+import vercel from "../../assets/images/skills_icons/vercel.svg";
+import netlify from "../../assets/images/skills_icons/netlify.png";
+import vsCode from "../../assets/images/skills_icons/vscode.png";
+// import postman from "../assets/images/skills_icons/postman.png";
+import adobe from "../../assets/images/skills_icons/adobe.svg";
+import figma from "../../assets/images/skills_icons/figma.png";
+
+export const skillTools = [
+  {
+    id: 1,
+    img: git,
+    name: "git",
+  },
+  {
+    id: 2,
+    img: github,
+    name: "github",
+  },
+  {
+    id: 3,
+    img: netlify,
+    name: "netlify",
+  },
+  {
+    id: 4,
+    img: vercel,
+    name: "vercel",
+  },
+  {
+    id: 5,
+    img: vsCode,
+    name: "VS Code",
+  },
+  //   {
+  //     id: 6,
+  //     img: postman,
+  //     name: "postman",
+  //   },
+  {
+    id: 6,
+    img: adobe,
+    name: "adobe XD",
+  },
+  {
+    id: 7,
+    img: figma,
+    name: "figma",
+  },
+];
+import verified from "../../assets/images/skills_icons/verified.png";
+
+// Personal Skills
+export const personalSkills = [
+  { id: 1, img: verified, name: "Communication" },
+  { id: 2, img: verified, name: "Design Sense" },
+  { id: 3, img: verified, name: "Teamwork" },
+  { id: 4, img: verified, name: "Structured Code" },
+  { id: 5, img: verified, name: "Honest" },
+];
