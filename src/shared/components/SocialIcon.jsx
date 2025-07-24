8776db9dd19aa75d6ad3ee517d79f28e9ec826e3
Nirob Sarker <nirobsarker012@gmail.com>
2025-07-24 11:31:49 +0600

    add: social-icons-links

--- src/shared/components/SocialIcon.jsx
+++ src/shared/components/SocialIcon.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const SocialIcon = () => {
-  return <div></div>;
+  const iconStyle =
+    "rounded-sm p-[6px] text-white transition-transform duration-300 hover:scale-110";
+
+  return (
+    <div className="flex items-center gap-3">
+      <h1 className="text-[20px] font-medium">Follow On</h1>
+      <Link
+        to="https://www.facebook.com/nirob.dmc.10"
+        target="_blank"
+        className={`cursor-pointer bg-[#1363DF] ${iconStyle}`}
+      >
+        <FaFacebookF size={20} />
+      </Link>
+      <Link
+        to="https://github.com/nirobsarker012"
+        target="_blank"
+        className={`cursor-pointer bg-[#000000] ${iconStyle}`}
+      >
+        <IoLogoGithub size={20} />
+      </Link>
+      <Link
+        to="https://www.linkedin.com/in/nirob-sarker-a9880b288/"
+        target="_blank"
+        className={`cursor-pointer bg-[#2155CD] ${iconStyle}`}
+      >
+        <FaLinkedinIn size={20} />
+      </Link>
+    </div>
+  );
 };
 
 export default SocialIcon;
